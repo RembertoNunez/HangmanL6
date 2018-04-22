d586a1e0bff2d85ddc2876fa82d27bc63d6a74e1
Remberto Nunez <remnunez@csumb.edu>
2018-04-22 09:47:58 +0000

    added files

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
-    <body>
+    <body style="background-color:powderblue;">
         <div class='container text-center'>
             <header>
                 <h1>Hangman</h1>
